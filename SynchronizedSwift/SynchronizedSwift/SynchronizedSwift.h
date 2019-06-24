3eb1a567e50721a562825fcceabac7bff0ae29a3
Christian Oberdörfer <oberdoerfer@quantum.qa>
2019-06-24 21:00:24 +0200

    Added project

---
+++ SynchronizedSwift/SynchronizedSwift/SynchronizedSwift.h
@@ -0,0 +1,19 @@
+//
+//  SynchronizedSwift.h
+//  SynchronizedSwift
+//
+//  Created by Christian Oberdörfer on 24.06.19.
+//  Copyright © 2019 Christian Oberdörfer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SynchronizedSwift.
+FOUNDATION_EXPORT double SynchronizedSwiftVersionNumber;
+
+//! Project version string for SynchronizedSwift.
+FOUNDATION_EXPORT const unsigned char SynchronizedSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SynchronizedSwift/PublicHeader.h>
+
+
